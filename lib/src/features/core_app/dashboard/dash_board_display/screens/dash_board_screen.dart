e74871b67a9b94fb434262f1b5e6ffb880690c50
Nyikwagh Moses <mosnyik@gmail.com>
2023-07-15 09:39:49 +0100

    A push so I can upgrade flutter

--- lib/src/features/core_app/dashboard/dash_board_display/screens/dash_board_screen.dart
+++ lib/src/features/core_app/dashboard/dash_board_display/screens/dash_board_screen.dart
@@ -12,7 +12,6 @@ import 'package:spacemall/src/features/core_app/store/data/store_repo.dart';
 import 'package:spacemall/src/features/core_app/store/screens/add_store.dart';
 
 import '../../../../../constants/text_strings.dart';
-import '../../dash_board_icon_screens/dash_board_receipts/application/reciepts_controller.dart';
 
 class DashBoard extends StatelessWidget {
   DashBoard({super.key});
